e9c794134989f386b76d5a40ceeaec52488d2564
Kevin Moore <kwmoore81@gmail.com>
2016-01-06 12:46:55 -0800

    First Upload

---
+++ Assets/Standard Assets/2D/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public float Health = 6;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    void Update()
+    {
+        if (transform.position.y <= fallBoundary)
+        {
+            DamagePlayer(Mathf.Infinity);
+        }
+    }
+    public void DamagePlayer (float damage)
+    {
+        playerStats.Health -= damage;
+        if (playerStats.Health <=0)
+        {
+            GameMaster.KillPlayer(this);
+        }
+    }
+}
---
+++ Assets/Standard Assets/2D/Sprites/GameMaster.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+    private AudioSource audioSource;
+
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public Transform spawnPrefab;
+
+    public IEnumerator RespawnPlayer()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(spawnDelay); 
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        GameObject clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+        Destroy(clone, 5f);
+
+    }
+
+	public static void KillPlayer (PlayerScript player)
+    {
+        Destroy (player.gameObject);
+        gm.StartCoroutine (gm.RespawnPlayer());
+    }
+}
